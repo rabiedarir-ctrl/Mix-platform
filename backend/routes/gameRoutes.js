b28afd746e63c560ac1a629a225f31adfde166b3
Mix platform <259675695+rabiedarir-ctrl@users.noreply.github.com>
2026-08-28 06:40:23 +0100

    Update gameRoutes.js

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -20,7 +20,23 @@ router.post('/btc/start', authenticateToken, async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+var  express  =  require ( 'express' ) ; 
+var  app  =  express ( ) ;
 
+// إعداد مُحدد معدل الطلبات: بحد أقصى خمسة طلبات في الدقيقة 
+var  RateLimit  =  require ( 'express-rate-limit' ) ; 
+var  limiter  =  RateLimit ( { 
+  windowMs : 15  *  60  *  1000 ,  // 15 دقيقة 
+  max : 100 ,  // بحد أقصى 100 طلب لكل windowMs 
+} ) ;
+
+
+// تطبيق محدد معدل الطلبات على جميع الطلبات app.use ( limiter ) ;
+
+app.get ( ' / : path ' , function ( req , res ) { let path = req.params.path ; if ( isValidPath ( path ) ) res.sendFile ( path ) ; } ) ;   
+     
+   
+    
 // -------------------------------
 // 🔹 تحديث حالة اللعبة
 router.post('/btc/update', authenticateToken, async (req, res) => {
@@ -39,7 +55,23 @@ router.post('/btc/update', authenticateToken, async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+var  express  =  require ( 'express' ) ; 
+var  app  =  express ( ) ;
 
+// إعداد مُحدد معدل الطلبات: بحد أقصى خمسة طلبات في الدقيقة 
+var  RateLimit  =  require ( 'express-rate-limit' ) ; 
+var  limiter  =  RateLimit ( { 
+  windowMs : 15  *  60  *  1000 ,  // 15 دقيقة 
+  max : 100 ,  // بحد أقصى 100 طلب لكل windowMs 
+} ) ;
+
+
+// تطبيق محدد معدل الطلبات على جميع الطلبات app.use ( limiter ) ;
+
+app.get ( ' / : path ' , function ( req , res ) { let path = req.params.path ; if ( isValidPath ( path ) ) res.sendFile ( path ) ; } ) ;   
+     
+   
+    
 // -------------------------------
 // 🔹 إنهاء جلسة اللعبة
 router.post('/btc/end', authenticateToken, async (req, res) => {
@@ -54,7 +86,23 @@ router.post('/btc/end', authenticateToken, async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+var  express  =  require ( 'express' ) ; 
+var  app  =  express ( ) ;
 
+// إعداد مُحدد معدل الطلبات: بحد أقصى خمسة طلبات في الدقيقة 
+var  RateLimit  =  require ( 'express-rate-limit' ) ; 
+var  limiter  =  RateLimit ( { 
+  windowMs : 15  *  60  *  1000 ,  // 15 دقيقة 
+  max : 100 ,  // بحد أقصى 100 طلب لكل windowMs 
+} ) ;
+
+
+// تطبيق محدد معدل الطلبات على جميع الطلبات app.use ( limiter ) ;
+
+app.get ( ' / : path ' , function ( req , res ) { let path = req.params.path ; if ( isValidPath ( path ) ) res.sendFile ( path ) ; } ) ;   
+     
+   
+    
 // -------------------------------
 // 🔹 التصدير
 module.exports = router;
