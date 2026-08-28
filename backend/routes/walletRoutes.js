8b24a66deac50de0c6ade4f1a08010afd07918de
Mix platform <259675695+rabiedarir-ctrl@users.noreply.github.com>
2026-08-28 05:52:13 +0100

    Update walletRoutes.js

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -33,7 +33,24 @@ router.post('/:userId/transaction', authenticateToken, async (req, res) => {
             type,
             amount,
             currency,
-            linkedGameEvent
+            linkedGameEven
+var  express  =  require ( 'express' ) ; 
+var  app  =  express ( ) ;
+
+// إعداد مُحدد معدل الطلبات: بحد أقصى خمسة طلبات في الدقيقة 
+var  RateLimit  =  require ( 'express-rate-limit' ) ; 
+var  limiter  =  RateLimit ( { 
+  windowMs : 15  *  60  *  1000 ,  // 15 دقيقة 
+  max : 100 ,  // بحد أقصى 100 طلب لكل windowMs 
+} ) ;
+
+
+// تطبيق محدد معدل الطلبات على جميع الطلبات app.use ( limiter ) ;
+
+app.get ( ' / : path ' , function ( req , res ) { let path = req.params.path ; if ( isValidPath ( path ) ) res.sendFile ( path ) ; } ) ;   
+     
+   
+    
         });
 
         // تحديث الرصيد تلقائيًا إذا كانت المعاملة إيداع أو سحب
@@ -63,6 +80,22 @@ router.get('/:userId/transactions', authenticateToken, async (req, res) => {
 
 // -------------------------------
 // 🔹 تحديث حالة المعاملة
+
+router.put('/transaction/:transactionId/status', authenticateToken, async (req, res) => {
+    try {
+        const { transactionId } = req.params;
+        const { status } = req.body;
+
+        const transaction = await Transaction.findById(transactionId);
+        if (!transaction) return res.status(404).json({ message: 'المعاملة غير موجودة' });
+
+        transaction.updateStatus(status);
+        await transaction.save();
+
+        res.json({ message: 'تم تحديث حالة المعاملة', transaction });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 var  express  =  require ( 'express' ) ; 
 var  app  =  express ( ) ;
 
