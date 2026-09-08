074185274510cd86d065ff4e447b04fe8fc7284a
Mix platform <259675695+rabiedarir-ctrl@users.noreply.github.com>
2026-09-08 19:53:22 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use(
    HEALTH
 ===================================================== */
 
-app.get("/health", async (req, res) => {
+app.get("api/health", async (req, res) => {
     const mongoose =
         require("mongoose");
 
